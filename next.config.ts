e0c79d75cb550bd6a81e661c2914b9c7f4bd3ded
Yanama <nolifen61@gmail.com>
2026-09-08 08:06:16 +0700

    bug cover

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-4c4801eca57e45a3b078b902bbce79a6.r2.dev',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
